397bf4a9774fca9f5593603c7ed90a0fd0d9a9f5
czp3009 <czp3009@gmail.com>
2017-07-02 23:20:23 +0800

    完成 首页 前端

--- src/main/java/com/hiczp/web/mystery/controller/HomeController.java
+++ src/main/java/com/hiczp/web/mystery/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+
 /**
  * Created by czp on 17-7-1.
  */
@@ -12,6 +14,7 @@ public class HomeController {
     @GetMapping("/")
     public ModelAndView index(ModelAndView modelAndView) {
         modelAndView.setViewName("/home/index");
-        return modelAndView;
+        //TODO 获得前八个图册
+        return modelAndView.addObject("books", Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
     }
 }
