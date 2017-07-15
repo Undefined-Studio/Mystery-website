457db38de39606c8208055b1fff3426ac503af18
czp3009 <czp3009@gmail.com>
2017-07-15 20:42:00 +0800

    使用 th:block

---
+++ src/main/java/com/hiczp/web/mystery/controller/PassController.java
@@ -0,0 +1,13 @@
+package com.hiczp.web.mystery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by czp on 17-7-15.
+ */
+@Controller
+@RequestMapping("/pass")
+public class PassController {
+
+}
